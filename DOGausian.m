6effbd423e3332efcf31d37012367bc06a9b4532
cs1100256@IITD.ERNET.IN <cs1100256@IITD.ERNET.IN@yourdomain.com>
2013-09-09 17:15:22 +0000

    It's something.

--- DOGausian.m
+++ DOGausian.m
@@ -1,34 +1,54 @@
 clc
 clear
 %--------------parameters-------------------%
-inputImageName = 'TestImages/minion in suit.jpg';
-outputImageName = 'TestImages/minion in suit.jpg';
+inputImageName = 'manOfSteel';
+outputImageName = '_artification.jpg';
+% yui = quantizationMedianCutDhruv(4,inputImageName);
 imageType = 'jpeg';
-sigma1 = 5;
-sigma2 = .5;
-kernelSize = 6;
+sigma = 1.6;
+tau = .998;
+k = 2;
+epi = 0;
+phi = 1;
+kernelSize = 5;
 %------------------------------------------------%
-a = imread(inputImageName,imageType);
-[m,n,o] = size(a);
+a = imread(strcat('TestImages/',inputImageName),imageType);
 figure,imshow(a);
+a = rgb2gray(a);
+[m,n,o] = size(a);
+d(:,:,:) = zeros(m,n,o);
 
-G1 = fspecial('Gaussian',[kernelSize,kernelSize],min(sigma1,sigma2));
-G2 = fspecial('Gaussian',[kernelSize,kernelSize],max(sigma1,sigma2));
+G1 = fspecial('Gaussian',[kernelSize,kernelSize],sigma);
+G2 = fspecial('Gaussian',[kernelSize,kernelSize],k*sigma);
 
 b1 = imfilter(a,G1,'symmetric');
 b2 = imfilter(a,G2,'symmetric');
-b = b1-b2;
-
-level = graythresh(b);
-c = im2bw(b,level);
-figure,imshow(c);
-
-d = c*255;
-for i = 1:3
-    e(:,:,i) = d;
+b = double(b1) - double(tau*b2);
+for p=1:m
+    for q=1:n
+        for r=1:o
+            if b(p,q,r) < epi
+                d(p,q,r) = 1;
+            else
+                d(p,q,r) = 1 + tanh(double(phi*b(p,q,r)));
+            end
+        end
+    end
 end
-e = uint8(e);
-yui = imread(outputImageName);
-ef = e+yui;
-figure,imshow(ef);
-imwrite(ef,'OutputImages/minion catoonization.jpg');
+minmin = min(min(min(d)));
+maxmax = max(max(max(d)));
+d = (d - minmin)/(maxmax-minmin);
+d = d*255;
+d = uint8(d);
+figure,imshow(d);
+
+% bwD = im2bw(d);
+% bwD = 1-bwD;
+% bm(:,:,1) = bwD;
+% bm(:,:,2) = bwD;
+% bm(:,:,3) = bwD;
+% figure,imshow(bm);
+% 
+% ef = uint8(bm).*yui;
+% figure,imshow(ef);
+% imwrite(ef,strcat('OutputImages/',inputImageName,outputImageName));
---
+++ New_DoG.m
@@ -0,0 +1,38 @@
+clc
+clear
+%--------------parameters-------------------%
+inputImageName = 'kate';
+outputImageName = '_artification.jpg';
+yui = quantizationMedianCutDhruv(3,inputImageName);
+imageType = 'jpeg';
+k = 2;
+sigma1 = 2.5;
+sigma2 = k*sigma1;
+tau = .998;
+thresh = 5;
+%------------------------------------------------%
+a = imread(strcat('TestImages/',inputImageName),imageType);
+figure,imshow(a);
+a = rgb2gray(a);
+[m,n,o] = size(a);
+d = zeros(m,n,o);
+
+G1 = fspecial('Gaussian',4*sigma1 + 1,sigma1);
+G2 = fspecial('Gaussian',4*sigma2 + 1,sigma2);
+
+b1 = imfilter(a,G1,'symmetric');
+b2 = imfilter(a,G2,'symmetric');
+b = double(b1) - double(tau*b2);
+% thresh = uint32(max(max(max(b))) + min(min(min(b))))*graythresh(b);
+
+d(b<thresh) = 1;
+d(b>=thresh) = 0;
+figure,imshow(d);
+
+bm(:,:,1) = d;
+bm(:,:,2) = d;
+bm(:,:,3) = d;
+
+ef = uint8(bm).*yui;
+figure,imshow(ef);
+imwrite(ef,strcat('OutputImages/',inputImageName,outputImageName));
